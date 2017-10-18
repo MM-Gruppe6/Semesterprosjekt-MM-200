9428031efb73afcefef0218e5aad3db7c312f56f
MM-Gruppe6 <31696845+MM-Gruppe6@users.noreply.github.com>
2017-10-18 19:21:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express()
 app.set('port', (process.env.PORT || 8080));
 
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('/public'));
 
 app.get('/', function(request, response) {
   response.render('pages/index');
