ff3b59aa0fe29cc43e4cdbd6bc2d4a5426b7cbc1
Stefan Blomberg <steblo94@gmail.com>
2017-10-18 20:14:47 +0200

    Ferdig, funnet bug

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
 
-app.listen(3000)
+app.set('port', (process.env.PORT || 8080));
+
+app.use(express.static('public'));
+app.listen(app.get('port'), function() {
+    console.log('Koblet opp til port', app.get('port'));
+});
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
