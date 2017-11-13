9f8094356ba565d8ef46c8f47dfa1805bc89a509
Charlotte Kasin <charlk15@uia.no>
2017-11-13 12:52:53 +0100

    opddatering

--- server/server.js
+++ server/server.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-const express = require('express')
-const app = express()
-
-app.use(express.static('public'))
-
-
-app.listen(8080, function () {
-  console.log('Listening on port 8080!')
-}) 
-=======
 var express = require('express')
 var app = express()
 
@@ -23,5 +12,3 @@ app.listen(app.get('port'), function() {
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
-
->>>>>>> ee29b15a7332f08efa0bf91386fa974adbf65e29
